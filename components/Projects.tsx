32acc1709dba800f7ffdadadedfd3f7123afceb0
A.Rahman Mufarrah <a.mufarrah@gmail.com>
2022-12-30 23:59:45 +0300

    editing project's component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion"
 import { Project } from '../typings';
 import { urlFor } from '../sanity';
+import Link from 'next/link';
 
 
 type Props = {
@@ -17,7 +18,7 @@ function Projects({ projects }: Props) {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
             className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full
-             justify-evenly mx-auto items-center z-0'>
+             justify-evenly mx-auto items-center z-0 '>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
 
             <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20
@@ -33,26 +34,26 @@ function Projects({ projects }: Props) {
                             viewport={{ once: true }}
                             src={urlFor(project?.image).url()} alt="" />
                         <div className='space-y-10 px-0 md:px-10 mx-w-6xl'>
-                            <h4 className='text-4xl font-semibold text-center'>
-                                <span className='underline decoration-[#EF476F]'>Case Study {i + 1} of {projects.length}:
+
+                            <h4 className='text-3xl font-semibold text-center text-[#EF476F]'>
+                                <span className=''>Case Study {i + 1} of {projects.length}:
                                 </span>{" "}
-                                {project?.title}
+                                <Link href={project?.linkToBuild}> <span className='underline'>{project?.title}</span></Link>
+
                             </h4>
-                            <div className='flex items-center space-x-2 justify-center'>
+                            <div className='flex items-center space-x-2 justify-center max-h-96'>
                                 {project?.technologies.map(technology => (
                                     <img className='h-10 w-10'
                                         key={technology?._id} src={urlFor(technology?.image).url()} alt="" />
                                 ))}
 
                             </div>
-
                             <p className='text-lg text-center md:text-left'>
                                 {project?.summary}
                             </p>
                         </div>
                     </div>
                 ))}
-
             </div>
 
             <div className='w-full absolute top-[30%] bg-[#EF476F]/10 left-0 h-[500px] -skew-y-12' />
@@ -61,3 +62,8 @@ function Projects({ projects }: Props) {
 }
 
 export default Projects
+
+
+{/* <Link className='flex items-center space-x-2 justify-center text-[#EF476F] underline' href={project?.linkToBuild}>
+Link to Project here
+</Link> */}
