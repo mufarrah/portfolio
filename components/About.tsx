9b35f260fa85917496b14441b74927d1cc7406ea
A.Rahman Mufarrah <a.mufarrah@gmail.com>
2023-01-06 02:33:54 +0300

    fixing responsive about

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ function About({ pageInfo }: Props) {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
             className='flex flex-col relative h-screen text-center max-w-7xl px-10 justify-evenly mx-auto items-center md:text-left md:flex-row'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+            <h3 className='absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
             <motion.img
                 initial={{
                     x: -200,
